be4d65ef7bed27c25661e5d6321159666ff45324
Ademir Bastiani <ademir@phipasa.local>
2020-01-22 17:36:59 -0300

    Criado seed do usuário Anounymous

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Anonymo',
+            'email' => 'anonymo@email.com',
+            'password' => bcrypt('123'),
+            'bibliography' => 'Usuário Anonymous'
+        ]);
+    }
+}
